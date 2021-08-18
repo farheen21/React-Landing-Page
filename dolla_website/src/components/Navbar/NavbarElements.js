8f2857d5833731ee2af5908ad31d0aa408c6bb95
Farheen khan <farheenfirdous12101999@gmail.com>
2021-08-18 09:59:04 -0700

    intial Commit

---
+++ dolla_website/src/components/Navbar/NavbarElements.js
@@ -0,0 +1,33 @@
+
+import styled from 'styled-components' ;
+import {Link as LinkR} from 'rect-router-dom';
+
+export const Nav = styled.nav`
+background: #000;
+height: 80px;
+margin-top: -80px;
+display: flex;
+justify-content : center;
+font-size: 1rem;
+position : sticky;
+top: 0 ;
+z-index: 10;
+
+@media screen and (max-width :960px) {
+    transition: 0.8sec all ease;
+}
+`
+
+export const NavbarContainer = styled.div`
+display: flex;
+justify-content: center;
+height: 80px;
+z-index: 1;
+width: 100%;
+padding: 0  24px;
+max-width: 1100px ;
+`
+
+export const Navlogo = styled{LinkR}'
+
+'
---
+++ dolla_website/src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Nav , NavbarContainer , Navlogo} from './NavbarElements';
+
+function Navbar() {
+    return (
+        <>
+        <Nav>
+            <NavbarContainer>
+            <Navlogo>Dolla</Navlogo>
+            </NavbarContainer>
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar;
+
