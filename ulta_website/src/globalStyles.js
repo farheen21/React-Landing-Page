e3cadb2772ca4db0cb1360e3153758965c714b2b
Farheen khan <farheenfirdous12101999@gmail.com>
2021-08-22 09:22:00 -0700

    Update globalStyles.js

--- ulta_website/src/globalStyles.js
+++ ulta_website/src/globalStyles.js
@@ -11,8 +11,20 @@ const createGlobalStyle = createGlobalStyle`
 `
 
 export const Container = styled.div`
+z-index: 1;
+width: 100%;
+max-width: 1300px;
+margin-right:auto;
+margin-left: auto;
+padding-right: 50px;
+padding-left: 50xp;
 
-`
+
+@media screen and (max-width: 991px) {
+padding-right: 30px;
+padding-left: 30xp;
+}
+`;
 
 
 
